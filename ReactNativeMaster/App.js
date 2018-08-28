98d11337f7e2818e392df80cf98261c6dda23937
Jordan Balintac <JordanBa@magenic.com>
2018-08-29 00:11:18 +0800

    Added set state and tying to component

--- ReactNativeMaster/App.js
+++ ReactNativeMaster/App.js
@@ -6,31 +6,32 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            showTheThing: true
+            showTheThing: true,
+            text: 'Edit me'
         }
-    
-      
       }
 
     clearText = () => {
-    this._textInput.setNativeProps({text: ''});
+        this.setState({text: ''})  /* (1) Use setState to change the component’s value */
     }
 
     hideText = () => {
-    this.setState({showTheThing: !this.state.showTheThing})
+        this.setState({showTheThing: !this.state.showTheThing}) /* (2) Use setState to show/hide a component */
     }
 
     render() {
         return (
             <View style={{flex: 1}}>
-            <Text>Text Input:</Text>
+                 
             {
                 this.state.showTheThing && 
 
                 <TextInput
                     ref={component => this._textInput = component}
                     style={{height: 50, flex: 1, marginHorizontal: 20, borderWidth: 1, borderColor: '#ccc'}}
-                />
+                    onChangeText={(text) => this.setState({text})}
+                    value={this.state.text} 
+                /> /* (3) Tie to state (25 points) */ /* (4) Added editing components to screen  */
             }
             
             <TouchableOpacity onPress={this.clearText}>
