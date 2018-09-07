7b346b0fb2ae15ec442798e2c1669ea82b5a63ab
Jordan Balintac <JordanBa@magenic.com>
2018-09-08 01:14:42 +0800

    assign 04

--- ReactNativeMaster/App.js
+++ ReactNativeMaster/App.js
@@ -1,158 +1,285 @@
 import React from 'react';
 import { 
-    ScrollView, 
-    Image, 
     Text, 
-    TouchableHighlight, 
-    View, 
+    Button, 
+    TextInput, 
+    TouchableOpacity, 
+    View,
     FlatList, 
-    Animated,
-    Easing } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+    ActivityIndicator, 
+    AsyncStorage,
+    StyleSheet,
+    Platform,
+    NetInfo
+} from 'react-native';
+import { createBottomTabNavigator  } from 'react-navigation';
 import { YellowBox } from 'react-native';
 
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+YellowBox.ignoreWarnings(['Warning: Failed child context type', 'Module RCTImageLoader']);
 
-class ListScreen extends React.Component {
+const SQLite = require('react-native-sqlite-storage')
+const db = Platform.select({
+    ios: () => SQLite.openDatabase({name: 'my.db', location: 'default'}, () =>  {},  () =>  {}),
+    android: () => SQLite.openDatabase({name: 'my.db', location: 'Library'},  () =>  {},  () =>  {}),
+})();
 
-    listData = [ 
-        { key: '1', name: 'Some person 1' }, 
-        { key: '2', name: 'Some person 2' }, 
-        { key: '3', name: 'Some person 3' }, 
-        { key: '4', name: 'Some person 4' }, 
-        { key: '5', name: 'Some person 5' }, 
-        { key: '6', name: 'Some person 6' }, 
-        { key: '7', name: 'Some person 7' }, 
-        { key: '8', name: 'Some person 8' } 
-    ];
 
-    renderItem({ item, index }) {
-       return (
-            /* 2. Have a new screen when a list item is tapped using touchable components (25 points) */
-            <TouchableHighlight
-                onPress={() => this.props.navigation.navigate('ScrollScreen')}>
-                <View style={{backgroundColor: 'white'}}>
-                    <Text>{item.name}</Text>
-                </View>
-            </TouchableHighlight>
-       )
+
+
+class NetworkScreen extends React.Component {
+
+
+    constructor(props){
+        super(props);
+        this.state ={ isLoading: true}
+      }
+    
+      componentDidMount(){
+        return fetch('https://launchlibrary.net/1.3/agency')
+          .then((response) => response.json())
+          .then((responseJson) => {
+    
+            this.setState({
+              isLoading: false,
+              dataSource: responseJson.agencies,
+            }, function(){
+    
+            });
+    
+          })
+          .catch((error) =>{
+            console.error(error);
+          });
+      }
+    
+    
+    
+      render(){
+    
+        if(this.state.isLoading){
+          return(
+            <View style={{flex: 1, padding: 20}}>
+              <ActivityIndicator/>
+            </View>
+          )
+        }
+    
+        return(
+          <View style={{flex: 1, paddingTop:20}}>
+            <FlatList
+              data={this.state.dataSource}
+              renderItem={({item}) => <Text>{item.name}, {item.countryCode}</Text>}
+              keyExtractor={(item, index) => index}
+            />
+          </View>
+        );
+      }
+}
+
+    
+
+class SQLiteScreen extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        };
     }
+
+    componentDidMount() {
+  
+
+        db.transaction((tx) => {
+            tx.executeSql('CREATE TABLE IF NOT EXISTS TextTable (text)');
+        });
+    }
+
+    _storeData = async () => {
+        try {
+            db.transaction(tx => {
+                tx.executeSql('DELETE FROM TextTable');    
+                tx.executeSql('INSERT INTO TextTable (text) values (?)', [this.state.text]);    
+            });
+        } catch (error) {
+          // Error saving data
+        }
+      }
+
+      _retrieveData = async () => {
+        try {
+            db.transaction(tx => {
+                tx.executeSql('SELECT text FROM TextTable', [], (tx, results) => {
+                    const value = results.rows.item(0).text;
+                    this.setState({
+                        text: value
+                    });
+                });
+            });
+
+         } catch (error) {
+           // Error retrieving data
+         }
+      }
 
     render() {
         return (
-            /* 1. Create a flat list or section list (25 points) */
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <FlatList
-                    data={this.listData}
-                    renderItem={this.renderItem.bind(this)}
+            <View style={{ flex: 1, alignItems: 'center'}}>
+
+                <Text>Save a text, change it and revert it back by Get Saved:</Text>
+                <TextInput
+                    onChangeText={(text) => this.setState({text})}
+                    value={this.state.text} 
+                /> 
+
+                <View style={styles.buttonSpacer} />
+                <Button
+                    title="Save"
+                    onPress={() => {
+                        this._storeData();
+                    }}
+                />
+                <View style={styles.buttonSpacer} />
+                <Button
+                    title="Get Saved"
+                    onPress={() => {
+                        this._retrieveData();
+                    }}
                 />
             </View>
         );
     }
 }
 
-class ScrollScreen extends React.Component {
+class AsyncScreen extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        };
+    }
+
+
+    _storeData = async () => {
+        try {
+          await AsyncStorage.setItem('text', this.state.text);
+        } catch (error) {
+          // Error saving data
+        }
+      }
+
+      _retrieveData = async () => {
+        try {
+          const value = await AsyncStorage.getItem('text');
+          if (value !== null) {
+            // We have data!!
+            console.log(value);
+
+            this.setState({
+                text: value
+            });
+
+
+          }
+         } catch (error) {
+           // Error retrieving data
+         }
+      }
     
     render() {
         return (
-            /* 3. The new screen should contain a scrollview with more contents than can fit on a screen (25 points) */
-            <ScrollView> 
-                <Text style={{fontSize:96}}>Scroll me plz</Text>
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Text style={{fontSize:96}}>If you like</Text>
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Text style={{fontSize:96}}>Scrolling down</Text>
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Text style={{fontSize:96}}>What's the best</Text>
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Text style={{fontSize:96}}>Framework around?</Text>
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-                <Text style={{fontSize:80}}>React Native</Text>
-            </ScrollView>
+            <View style={{ flex: 1, alignItems: 'center'}}>
+
+                <Text>Save a text, change it and revert it back by Get Saved:</Text>
+                <TextInput
+                    onChangeText={(text) => this.setState({text})}
+                    value={this.state.text} 
+                /> 
+
+                <View style={styles.buttonSpacer} />
+                <Button
+                    title="Save"
+                    onPress={() => {
+                        this._storeData();
+                    }}
+                />
+                <View style={styles.buttonSpacer} />
+                <Button
+                    title="Get Saved"
+                    onPress={() => {
+                        this._retrieveData();
+                    }}
+                />
+            </View>
         );
     }
 }
 
-const transitionConfig = () => {
-    return {
-      transitionSpec: {
-        duration: 750,
-        easing: Easing.out(Easing.poly(4)),
-        timing: Animated.timing,
-        useNativeDriver: true,
-      },
-      screenInterpolator: sceneProps => {
-          const { position, layout, scene, index, scenes } = sceneProps
-          const toIndex = index
-          const thisSceneIndex = scene.index
-          const height = layout.initHeight
-          const width = layout.initWidth
+class OfflineScreen extends React.Component {
     
-          const translateX = position.interpolate({
-            inputRange: [thisSceneIndex - 1, thisSceneIndex, thisSceneIndex + 1],
-            outputRange: [width, 0, 0]
-          })
-    
-          // Since we want the card to take the same amount of time
-          // to animate downwards no matter if it's 3rd on the stack
-          // or 53rd, we interpolate over the entire range from 0 - thisSceneIndex
-          const translateY = position.interpolate({
-            inputRange: [0, thisSceneIndex],
-            outputRange: [height, 0]
-          })
-    
-          const slideFromRight = { transform: [{ translateX }] }
-          const slideFromBottom = { transform: [{ translateY }] }
-    
-          const lastSceneIndex = scenes[scenes.length - 1].index
-    
-          // Test whether we're skipping back more than one screen
-          if (lastSceneIndex - toIndex > 1) {
-            // Do not transoform the screen being navigated to
-            if (scene.index === toIndex) return
-            // Hide all screens in between
-            if (scene.index !== lastSceneIndex) return { opacity: 0 }
-            // Slide top screen down
-            return slideFromBottom
-          }
-    
-          return slideFromRight
-        },
-    }}
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            NetworkStatus: ''
+        }
+    }
 
-const RootStack = createStackNavigator(
-    {
-        ListScreen: ListScreen,
-        ScrollScreen: ScrollScreen,
-    },
-    {
-      initialRouteName: 'ListScreen',
-      transitionConfig, /* 4. Animate the showing or removing of the new screen (25 points) */
-    });
-  
-export default class App extends React.Component {
+    componentDidMount() {
+        NetInfo.isConnected.fetch().then(hasInternetConnection => {
+            this.setState({NetworkStatus: hasInternetConnection ? 'Online' : 'Offline'})
+          });
+
+        NetInfo.isConnected.addEventListener('connectionChange', hasInternetConnection => {
+           this.setState({NetworkStatus: hasInternetConnection ? 'Online' : 'Offline'})
+        });
+    }
+    
     render() {
-        return <RootStack />;
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Off wifi and mobile network to change status:</Text>
+                <Text>{ this.state.NetworkStatus }</Text>
+            </View>
+        );
     }
 }
+
+
+
+export default createBottomTabNavigator({
+    
+    NetworkScreen: {
+        screen: NetworkScreen,
+        navigationOptions: {tabBarLabel: "Network"},
+    },
+    SQLiteScreen: {
+        screen: SQLiteScreen,
+        navigationOptions: {tabBarLabel: "SQLite"},
+    },
+    AsyncScreen: {
+        screen: AsyncScreen,
+        navigationOptions: {tabBarLabel: "Async"},
+    },
+    OfflineScreen: {
+        screen: OfflineScreen,
+        navigationOptions: {tabBarLabel: "Offline"},
+    }
+});
+
+
+const styles = StyleSheet.create({
+    mainView: {
+        flex: 1,
+        flexDirection: "column",
+        backgroundColor: "#BBDEFB"
+    },
+    topSpacer: {
+        flex: 0.3
+    },
+    buttonSpacer: {
+        flex: 0.2
+    }
+});
