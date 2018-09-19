c9fbb71343f2e6a61b14954b7fe0cb74214b7cc8
Jordan Balintac <JordanBa@magenic.com>
2018-09-19 16:56:22 +0800

    notification

--- ReactNativeMaster/android/app/src/main/java/com/reactnativemaster/MainApplication.java
+++ ReactNativeMaster/android/app/src/main/java/com/reactnativemaster/MainApplication.java
@@ -3,6 +3,7 @@ package com.reactnativemaster;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new SQLitePluginPackage(),   // register SQLite Plugin here
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new RNCameraPackage()
       );
     }
